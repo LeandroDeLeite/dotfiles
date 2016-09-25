7689895d4a7db701ef65d129ec8c309647106bce
leandrodeleite <leandrodeleite@gmail.com>
2016-09-25 12:15:27 -0300

    Format print for Upgrade, Upgrade, Clean commands

--- sup.sh
+++ sup.sh
@@ -1,12 +1,17 @@
-echo ">> Update -----------"
-sudo apt-get update       -y
-echo ">> Upgrade ----------"
-sudo apt-get upgrade      -y
-echo ">> Dist-Upgrade -----"
+echo "Update"
+sudo apt-get update -y
+echo ""
+echo "Upgrade"
+sudo apt-get upgrade -y
+echo ""
+echo "Dist-Upgrade"
 sudo apt-get dist-upgrade -y
-echo ">> Clean ------------"
-sudo apt-get clean        -y
-echo ">> Autoclean---------"
-sudo apt-get autoclean    -y
-echo ">> Autoremove--------"
-sudo apt-get autoremove   -y
+echo ""
+echo "Clean"
+sudo apt-get clean -y
+echo ""
+echo "Autoclean"
+sudo apt-get autoclean -y
+echo ""
+echo "Autoremove"
+sudo apt-get autoremove -y
