89ad4b184d39719ccce7024cb5f0197acd761b57
leandrodeleite <leandrodeleite@gmail.com>
2016-09-24 22:12:08 -0300

    Print Update, Upgrade, Clean commands

--- sup.sh
+++ sup.sh
@@ -1,6 +1,12 @@
+echo ">> Update -----------"
 sudo apt-get update       -y
+echo ">> Upgrade ----------"
 sudo apt-get upgrade      -y
+echo ">> Dist-Upgrade -----"
 sudo apt-get dist-upgrade -y
+echo ">> Clean ------------"
 sudo apt-get clean        -y
+echo ">> Autoclean---------"
 sudo apt-get autoclean    -y
+echo ">> Autoremove--------"
 sudo apt-get autoremove   -y
